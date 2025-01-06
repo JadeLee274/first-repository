57b651ac6ce6b9968fc9139330a17b70e3deb1e3
JadeLee274 <tmdals274@snu.ac.kr>
2025-01-06 13:25:48 +0900

    I am new to git

--- git_test.py
+++ git_test.py
@@ -1,2 +1,10 @@
-print("Hello World")
 import numpy as np
+
+def is_prime(n: int) -> bool:
+    if n == 1 or n == 2:
+        return False
+    else:
+        for i in range(2, int(np.sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
